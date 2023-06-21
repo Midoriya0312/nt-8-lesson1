46b2daa0a46b9f7f9d6ffacba642ec7cf28fb91c
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-06-21 11:16:33 +0500

    lesson1

---
+++ index.js
@@ -0,0 +1,316 @@
+// 213
+// "dads"
+// true
+// false
+// null
+// undefined
+// {}
+// {
+//     name: 'Link',
+//     color: 'blue',
+//     date: '2020-10-10',
+//     price: null,
+//     outdate: false,
+// }
+// {true, null, false, 3, undefined, {}, [], 'dasd'};
+
+
+// var age = 19;
+
+// if(age >= 18) {
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+// 1 misol
+
+// var a = 1;
+
+// if(a >= 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 2,3 misol 
+
+// var n = 11;
+
+// if (n % 2 !== 0) {
+//     console.log("toq son");
+// } else {
+//     console.log("juft son");
+// }
+
+// 4 misol
+
+// var a = 3;
+// var b = 5;
+
+// if(a > 0 && b > 0){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+// 5 misol
+
+// var a = 5;
+// var b = 3;
+// var c = 1;
+
+// if(a > b && b > c){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 6 misol 
+
+// var a = 1;
+// var b = 4;
+// var c = 6;
+
+// if(a < b && b > c){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// 7 misol 
+
+// var a = 11;
+// var b = 11;
+// if (a % 2 !== 0 && b & 2 !== 0) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// 8 misol
+
+// var a = 3;
+// var b = 4;
+// var c = 1;
+// var d = 7;
+
+// if(a >= 0 && b >= 0 && c >= 0 && d >= 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// 9 misol
+
+// var a = -4;
+// var b = -2;
+// var c = 5;
+
+// if(a >= 0 && b >= 0 || b >= 0 && c >= 0 || a >= 0 && c >= 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// 10 misol
+
+// var a = 4;
+
+// if(a == 1){
+//     console.log('dushanba');
+// } else if(a == 2){
+//     console.log('seshanba');
+// }else if(a == 3){
+//     console.log('chorshanba');
+// }else if(a == 4){
+//     console.log('payshanba');
+// }else if(a == 5){
+//     console.log('juma');
+// }else if(a == 6){
+//     console.log('shanba');
+// }else if(a == 7){
+//     console.log('yakshanba');
+// }
+
+// 11 misol
+
+// var a = 91;
+
+// if(a == 99){
+//     console.log('uzmobile');
+// }else if(a == 90 || a == 91){
+//     console.log('beeline');
+// }else if(a == 71){
+//     console.log('Gorodskoy');
+// }else if(a == 98){
+//     console.log('perfectum');
+// }else if(a == 94){
+//     console.log('\');
+// }
+
+// 12 misol
+
+// var a = -1;
+
+// if(a >= 0){
+//     a++
+// } else{
+//     a--
+// }
+
+// console.log(a);
+
+// 13 misol
+
+// var a = -3;
+
+// if(a >= 0){
+//     a += 3
+// }else{
+//     a -= 2
+// }
+
+// console.log(a);
+
+// 14 misol
+
+// var a = 7;
+// var b = 5;
+
+// if(a > b){
+//     console.log(a);
+// }else if(b > a){
+//     console.log(b);
+// }
+
+// 15 misol
+
+// var a = 4;
+// var b = 1;
+// var c = 5;
+
+// if(a > b && a > c){
+//     console.log(a);
+// }else if(b > a && b > c){
+//     console.log(b);
+// }else if(c > a && c > b){
+//     console.log(c);
+// }
+
+// 16 misol
+
+// var a = 4;
+// var b = 1;
+// var c = 5;
+
+// if(a < b && a < c){
+//     console.log(a);
+// }else if(b < a && b < c){
+//     console.log(b);
+// }else if(c < a && c < b){
+//     console.log(c);
+// }
+
+// 17 misol
+
+// var a = 'male';
+
+// if(a == 'female'){
+//     console.log('siz qiz bolasiz');
+// }else{
+//     console.log('siz oqil bolasiz');
+// }
+
+// 18 misol
+
+// var a = 5;
+
+// if(a == 5){
+//     console.log('siz stependiya ololisiz');
+// }else if(a == 4){
+//     console.log('siz stependiya ololisiz');
+// }else{
+//     console.log('stependiya olomisiz');
+// }
+
+// 19 misol yechilmadi
+
+// var a = 4;
+
+// if(a % 2 != 0 > 0){
+//     console.log(true);
+// }
+
+
+
+// 20 misol yechilmadi
+
+// var a = 2006;
+// var b = 2023;
+
+// var c = b - a;
+
+// if(c){
+//     console.log(c);
+// }
+
+
+// 21 misol
+
+// var a = 1;
+// var b = 6;
+// var c = 8;
+
+// if(a <= b && b <= c){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 22 misol
+
+// var a = 8;
+// var b = 7;
+
+// if(a % 2 !== 0){
+//     console.log(a);
+// }else if(b % 2 !== 0){
+//     console.log(b);
+// }
+
+
+// 23 misol
+
+// var a = 4;
+// var b = 1;
+// var c = 6;
+
+// if(a > 0 && b > 0 && c > 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// 24 misol
+
+
+// var a = -3;
+// var b = -2;
+// var c = 6;
+
+// if(a > 0){
+//     console.log(a);
+// }else if(b > 0){
+//     console.log(b);
+// }else if(c > 0){
+//     console.log(c);
+// }else{
+//     console.log(false);
+// }
+
